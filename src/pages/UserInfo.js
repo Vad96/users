ba35705ed354dfbd513d0b92b02c553c0377fedc
Vadym Stavnychyi <vadim96dk@gmail.com>
2020-12-10 16:56:30 +0200

    show user details

--- src/pages/UserInfo.js
+++ src/pages/UserInfo.js
@@ -1,7 +1,31 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
 
 function UserInfo() {
-  return <main className="main">user info</main>;
+  const { id } = useParams();
+  const { users } = useSelector((state) => state.users);
+  const user = users.results.find((item) => item.id.name === id);
+  console.log(user);
+  return (
+    <main className="main">
+      <ul>
+        <li>
+          <img src={user.picture.medium} alt={user.name.first} />
+        </li>
+        <li>
+          {user.name.first} {user.name.last}
+        </li>
+        <li>{user.email}</li>
+        <li>
+          {user.location.city}, {user.location.country}
+        </li>
+        <li>{user.gender}</li>
+        <li>{new Date(user.dob.date).toDateString()}</li>
+        <li>phone: {user.phone}</li>
+      </ul>
+    </main>
+  );
 }
 
 export default UserInfo;
