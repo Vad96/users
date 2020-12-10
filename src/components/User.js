0e23d0bfdeb769ad1fe7ad1bb915e62a67d724bc
Vadym Stavnychyi <vadim96dk@gmail.com>
2020-12-10 16:31:51 +0200

    show users

---
+++ src/components/User.js
@@ -0,0 +1,10 @@
+// import React from "react";
+
+// function User({user}) {
+//   return (<li>
+//     {user.name && <img src={user.picture.large} alt=""/>}
+//      <div>{user.name.first}</div>
+//   </li>)
+// }
+
+// export default User;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
 import { getUsers } from "../redux/actions/actions";
+import User from "../components/User";
 
 function Home() {
   const { users, pickedUsers } = useSelector((state) => state.users);
@@ -9,8 +11,25 @@ function Home() {
   useEffect(() => {
     dispatch(getUsers());
   }, []);
-  console.log(users);
-  return <main className="main">Home</main>;
+
+  return (
+    <main className="main">
+      <div className="usersList">
+        {users.results &&
+          users.results.map((user) => {
+            return (
+              <Link to={`/user/${user.id.name}`} key={user.email}>
+                <img src={user.picture.medium} alt={user.name.first} />
+                <div>
+                  {user.name.first} {user.name.last}
+                </div>
+                <button>Add</button>
+              </Link>
+            );
+          })}
+      </div>
+    </main>
+  );
 }
 
 export default Home;
