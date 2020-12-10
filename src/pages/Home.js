c02d28f400f8304d781ca20df95110dc35abb372
Vadym Stavnychyi <vadim96dk@gmail.com>
2020-12-10 13:07:38 +0200

    added useselector

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,8 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 function Home() {
+  const { users, pickedUsers } = useSelector((state) => state.users);
   return <main className="main">Home</main>;
 }
 
--- src/redux/reducers/usersReducer.js
+++ src/redux/reducers/usersReducer.js
@@ -1,8 +1,10 @@
-const initialState = [];
+const initialState = { users: ['ld'], pickedUsers: ['ldsk'] };
 
 export function usersReducer(state = initialState, action) {
   switch (action.type) {
     case "GET_USERS":
+      return action.payload;
+    case "ADD_USER":
       return action.payload;
     default:
       return state;
