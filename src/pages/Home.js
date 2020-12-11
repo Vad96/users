67656f03638db3498eb19962034b37c0a4229b1a
Vadym Stavnychyi <vadim96dk@gmail.com>
2020-12-11 16:03:23 +0200

    tabs

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { _ } from "lodash";
 import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
@@ -7,6 +7,7 @@ import User from "../components/User";
 import getTime from "../helpers/getTime";
 
 function Home() {
+  const [activeTab, setActiveTab] = useState("1");
   const { users, pickedUsers, usersCount } = useSelector(
     (state) => state.users
   );
@@ -18,45 +19,63 @@ function Home() {
   }, [usersCount]);
 
   return (
-    <main className="main">
-      <div className="usersList">
-        {getTime()}
-        {users.results && !loading ? 
-          users.results.map((user) => {
-            return (
-              <div key={user.email}>
-                <Link to={`/user/${user.id.name}`}>
-                  <img src={user.picture.medium} alt={user.name.first} />
-                  <div>
-                    {user.name.first} {user.name.last}
-                  </div>
-                </Link>
-                <button onClick={() => dispatch(addUser(user))}>Add</button>
-              </div>
-            );
-          }) : <div>loading...</div>}
-        <button onClick={() => dispatch(loadMoreUsers())}>Load more</button>
-      </div>
+    <>
+      <main className="main">
+        <button
+          style={{ display: "inline-block" }}
+          type="button"
+          onClick={() => setActiveTab("1")}
+        >
+          Tab 1
+        </button>
+        <button
+          style={{ display: "inline-block" }}
+          type="button"
+          onClick={() => setActiveTab("2")}
+        >
+          Tab 2
+        </button>
+        <div className={activeTab === "1" ? "usersList" : "hideTab"}>
+          {users.results && !loading ? (
+            users.results.map((user) => {
+              return (
+                <div key={user.email}>
+                  <Link to={`/user/${user.id.name}`}>
+                    <img src={user.picture.medium} alt={user.name.first} />
+                    <div>
+                      {user.name.first} {user.name.last}
+                    </div>
+                  </Link>
+                  <button onClick={() => dispatch(addUser(user))}>Add</button>
+                </div>
+              );
+            })
+          ) : (
+            <div>loading...</div>
+          )}
+          <button onClick={() => dispatch(loadMoreUsers())}>Load more</button>
+        </div>
 
-      <div className="pickedUsers">
-        {pickedUsers ? (
-          pickedUsers.map((user) => {
-            return (
-              <div key={user.email}>
-                <Link to={`/user/${user.id.name}`}>
-                  <img src={user.picture.medium} alt={user.name.first} />
-                  <div>
-                    {user.name.first} {user.name.last}
-                  </div>
-                </Link>
-              </div>
-            );
-          })
-        ) : (
-          <div>No users yet...</div>
-        )}
-      </div>
-    </main>
+        <div className={activeTab === "2" ? "pickedUsers" : "hideTab"}>
+          {pickedUsers ? (
+            pickedUsers.map((user) => {
+              return (
+                <div key={user.email}>
+                  <Link to={`/user/${user.id.name}`}>
+                    <img src={user.picture.medium} alt={user.name.first} />
+                    <div>
+                      {user.name.first} {user.name.last}
+                    </div>
+                  </Link>
+                </div>
+              );
+            })
+          ) : (
+            <div>No users yet...</div>
+          )}
+        </div>
+      </main>
+    </>
   );
 }
 
