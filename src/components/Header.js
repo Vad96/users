66131bd3474e781c346774adf69ab938c1e8a733
Vadym Stavnychyi <vadim96dk@gmail.com>
2020-12-11 13:06:19 +0200

    show users amount in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { Link, useHistory } from "react-router-dom";
+import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import getTime from "../helpers/getTime";
 
 function Header(props) {
   const [time, setTime] = useState("");
+  const { pickedUsers } = useSelector((state) => state.users);
   let history = useHistory();
 
   useEffect(() => {
@@ -18,6 +20,7 @@ function Header(props) {
     <header className="header">
       Header
       <div>{time}</div>
+      Users amount: {pickedUsers && pickedUsers.length}
     </header>
   );
 }
