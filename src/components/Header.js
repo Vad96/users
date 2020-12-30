e251268c4d90a2232955c34a445d43056a559cfa
Vadym Stavnychyi <vadim96dk@gmail.com>
2020-12-30 13:41:42 +0200

    localization

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,18 +2,25 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import getTime from "../helpers/getTime";
+import { useTranslation } from "react-i18next";
 
 function Header(props) {
   const [time, setTime] = useState("");
   const { pickedUsers } = useSelector((state) => state.users);
   const location = window.location.pathname.split("/");
-
+  const { t, i18n } = useTranslation();
+ 
   useEffect(() => {
     const interval = setInterval(() => {
       setTime(getTime());
     }, 250);
     return () => clearInterval(interval);
   }, []);
+
+  const setLanguage = (lang) => {
+    i18n.changeLanguage(lang);
+    console.log(lang)
+  };
 
   return (
     <header className="header">
@@ -23,8 +30,12 @@ function Header(props) {
         </Link>
       )}
       <div>
-        <strong>Current date: </strong>
+        <strong>{t('date.1')}: </strong>
         {time}
+      </div>
+      <div>
+        <button onClick={() => setLanguage("en")}>en</button>
+        <button onClick={() => setLanguage("uk")}>uk</button>
       </div>
       Users count: {pickedUsers && pickedUsers.length}
     </header>
--- src/index.js
+++ src/index.js
@@ -1,29 +1,32 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
-import thunk from 'redux-thunk';
-import createSagaMiddleware from 'redux-saga';
+import thunk from "redux-thunk";
+import createSagaMiddleware from "redux-saga";
 import { composeWithDevTools } from "redux-devtools-extension";
-import rootReducer from './redux/reducers/index';
-import {sagaWatcher} from './redux/sagas/sagas';
+import rootReducer from "./redux/reducers/index";
+import { sagaWatcher } from "./redux/sagas/sagas";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import "./i18n/i18next";
 
 const saga = createSagaMiddleware();
 
-const store = createStore(rootReducer, composeWithDevTools(
-  applyMiddleware(thunk, saga)
-  )
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk, saga))
 );
 
-saga.run(sagaWatcher)
+saga.run(sagaWatcher);
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <Suspense fallback={<div>Loading...</div>}>
+        <App />
+      </Suspense>
     </BrowserRouter>
   </Provider>,
   document.getElementById("root")
