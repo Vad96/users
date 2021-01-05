f74632e9daa5ca1e8ad98f243cfef4001b4331de
Vadym Stavnychyi <vadim96dk@gmail.com>
2021-01-05 13:23:07 +0200

    refactor tabs

--- src/components/tabs/Tabs.js
+++ src/components/tabs/Tabs.js
@@ -1,34 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Tab from "./Tab";
 import TabButtons from "./TabButtons";
 
-class Tabs extends React.Component {
-  state = {
-    activeTab: this.props.children[0].props.label,
-  };
-  changeTab = (tab) => {
-    this.setState({ activeTab: tab });
-  };
-  render() {
-    let content;
-    let buttons = [];
-    return (
-      <div>
-        {React.Children.map(this.props.children, (child) => {
-          buttons.push(child.props.label);
-          if (child.props.label === this.state.activeTab)
-            content = child.props.children;
-        })}
+function Tabs(props) {
+  const [activeTab, setActiveTab] = useState(props.children[0].props.label);
 
-        <TabButtons
-          activeTab={this.state.activeTab}
-          buttons={buttons}
-          changeTab={this.changeTab}
-        />
-        <div className="tab-content">{content}</div>
-      </div>
-    );
-  }
+  const changeTab = (tab) => {
+    setActiveTab(tab);
+  };
+
+  let content;
+  let buttons = [];
+  return (
+    <div>
+      {React.Children.map(props.children, (child) => {
+        buttons.push(child.props.label);
+        if (child.props.label === activeTab) content = child.props.children;
+      })}
+
+      <TabButtons
+        activeTab={activeTab}
+        buttons={buttons}
+        changeTab={changeTab}
+      />
+      <div className="tab-content">{content}</div>
+    </div>
+  );
 }
 
 export default Tabs;
