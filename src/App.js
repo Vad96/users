e85a1e69c2458f94d4f035e7371b4b5dd5060dd7
Vadym Stavnychyi <vadim96dk@gmail.com>
2021-01-08 14:57:59 +0200

    lazy suspense

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { Switch, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
-import UserInfo from "./pages/UserInfo";
 import Footer from "./components/Footer";
 import NotFound from "./pages/NotFound";
 import ErrorBoundary from "./components/ErrorBoundary";
 import "./App.scss";
+const UserInfo = lazy(() => import("./pages/UserInfo"));
 
 function App() {
   return (
@@ -15,7 +15,9 @@ function App() {
         <Header />
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route path="/user/:id" component={UserInfo} />
+          <Suspense fallback={<div>Завантаження...</div>}>
+            <Route path="/user/:id" component={UserInfo} />
+          </Suspense>
           <Route path="*" component={NotFound} />
         </Switch>
         <Footer />
